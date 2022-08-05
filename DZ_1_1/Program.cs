acecdba4d8c5fc3dbbb47e3f3a4e4ecf68814dae
kapex52 <kapex@bk.ru>
2022-08-05 17:44:57 +0300

    дз 1

--- DZ_1_1/Program.cs
+++ DZ_1_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введте 1 число");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введте 2 число");
+int number2=Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+
+{
+   Console.WriteLine(number1 + " равно " + number2);
+}
+if (number1 > number2)
+{
+    Console.WriteLine(number1 + " болше " + number2);
+}
+if (number1 < number2)
+{
+    Console.WriteLine(number1 + " меньше " + number2);
+}
--- DZ_1_2/Program.cs
+++ DZ_1_2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введте 1 число");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введте 2 число");
+int number2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введте 3 число");
+int number3=Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number1 == number2)
+    {
+if (number3 == number2)
+Console.WriteLine(" все равны" + max);
+    }
+
+if (number1 < number2)
+{
+   if (number2 < number3)
+   {
+    max=number3;
+    Console.WriteLine(" наиболшее число  " + max);
+   }
+    max=number2;
+    Console.WriteLine(" наиболшее число  " + max);
+}
+else
+if (number1 > number3)
+{
+    Console.WriteLine(" наиболшее число  " + max);
+}
+
+   // max=number3;
+   // Console.WriteLine(" наиболшее число  " + max);
