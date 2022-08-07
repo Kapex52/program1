c4d148b85f90d7aa53deee8c2e08ce7b33186596
kapex52 <kapex@bk.ru>
2022-08-07 09:36:02 +0300

    dz_1-4

--- DZ_1_4/Program.cs
+++ DZ_1_4/Program.cs
@@ -1,5 +1,17 @@
 ﻿using System;
 Console.WriteLine("Введте 1 число");
 int number1=Convert.ToInt32(Console.ReadLine());
-int i=2
-if (i <= number1)
+if (number1 > 0)
+{
+for (int i = 1; i <= number1; i++)
+    if (i%2==0)
+    {
+    Console.WriteLine("Четные числа " + i);
+    }
+}
+
+else
+{
+    Console.WriteLine("вычесление не возможно введеное число 0 или отрицательное " );
+}
+
