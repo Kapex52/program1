3b7c29404df1968e07337312984671a924073d50
kapex52 <kapex@bk.ru>
2022-08-08 11:43:09 +0300

    12

--- проба1/Program.cs
+++ проба1/Program.cs
@@ -1,4 +1,15 @@
-﻿Console.Write("Ввкдите имя");
-string username = Console.ReadLine();
-Console.Write("Привет,");
-Console.Write(username);
+﻿using System;
+Console.WriteLine("Введте 1 число");
+int number1=Convert.ToInt32(Console.ReadLine());
+int prov=0;
+
+while (prov=number1%2==0)
+{
+    Console.WriteLine(" чило " + number1 + " четное");
+    prov++;
+}
+
+// else
+// {
+//     Console.WriteLine(" чило " + number1 + " НЕ четное");
+// }
