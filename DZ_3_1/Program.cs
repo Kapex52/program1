dcaa2e91ee38daf62fac94bba4a3ae9e9222b418
kapex52 <kapex@bk.ru>
2022-08-15 16:19:10 +0300

    дз3

---
+++ DZ_3_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+int i = 0, size = number.Length - 1;
+while ((i < size) && (number[i] == number[size]))
+{ i++; size--; }
+if (number[i] == number[size])
+    Console.WriteLine("Введенное число: " + "\t" + number + "\t" + " палиндром");
+else
+    Console.WriteLine("Введенное число: " + "\t" + number + "\t" + " НЕ палиндром");
+
