cb0b2313aaa3174069a040247a43a97299abf42d
kapex52 <kapex@bk.ru>
2022-08-20 20:10:02 +0300

    DZ5 final

--- DZ_C/DZ_5/DZ_5_1/Program.cs
+++ DZ_C/DZ_5/DZ_5_1/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System.Drawing;
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 //  Напишите программу, которая покажет количество чётных чисел в массиве.
-
 // [345, 897, 568, 234] -> 2
-
 
 Console.WriteLine("Введите число элеменов масива:");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] array = new int [size];
-
 int count = 0;
-
 for (int i = 0; i < size; i++)
 {
     array[i] = new Random().Next(100,1000);
--- DZ_C/DZ_5/DZ_5_2/Program.cs
+++ DZ_C/DZ_5/DZ_5_2/Program.cs
@@ -1,26 +1,21 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
-
 // [3, 7, 23, 12] -> 19
-
 // [-4, -6, 89, 6] -> 0
-
 
 Console.WriteLine("Введите число элеменов масива:");
 int size = Convert.ToInt32(Console.ReadLine());
-int[] array = new int [size];
-
+int[] array = new int[size];
 int sum = 0;
-
 for (int i = 0; i < size; i++)
 {
     array[i] = new Random().Next(100);
     Console.Write(array[i] + " ");
 
 }
-for (int j = 1; j < size; j+=2)
+for (int j = 1; j < size; j += 2)
 {
-    sum=sum + array[j];
+    sum = sum + array[j];
 }
 Console.WriteLine();
 Console.WriteLine($"сумма элементов, стоящих на нечётных позициях {sum}");
