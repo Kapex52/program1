6a0fa4173d64e3290a954ff2bc6503e3914739ce
kapex52 <kapex@bk.ru>
2022-08-25 19:48:44 +0300

    2

--- DZ_C/DZ_6/DZ_6_1/Program.cs
+++ DZ_C/DZ_6/DZ_6_1/Program.cs
@@ -39,25 +39,40 @@
 //             Console.ReadLine();
 //         }
 
- static void Main(string[] args)
-        {
-            int[] mas = new int[100];
-            int i = 0; 
-            string test;
-            int count = 0;
+//  static void Main(string[] args)
+//         {
+//             int[] mas = new int[100];
+//             int i = 0; 
+//             string test;
+//             int count = 0;
  
-            do
-            {
-                test = Console.ReadLine();
-                if (test == "end") break;
-                mas[i] = Convert.ToInt32(test);
-                ++i; ++count;
-            } while (true);
+//             do
+//             {
+//                 test = Console.ReadLine();
+//                 if (test == "end") break;
+//                 mas[i] = Convert.ToInt32(test);
+//                 ++i; ++count;
+//             } while (true);
             
-            Console.WriteLine("Vivod: ");
-            for (int j = 0; j < count; j++)
-            {
-                Console.Write("{0} ",mas[j]);
-            }
-            Console.ReadLine();
-        }
+//             Console.WriteLine("Vivod: ");
+//             for (int j = 0; j < count; j++)
+//             {
+//                 Console.Write("{0} ",mas[j]);
+//             }
+//             Console.ReadLine();
+//         }
+
+Console.Write("Введите элементы(через пробел): ");
+String[] arr = Console.ReadLine().Split();
+int[] arr2 = int.Parse(arr);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
