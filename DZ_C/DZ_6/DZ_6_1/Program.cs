e3fea9d0f77521e4153b6b037e3a02780f33c1bb
kapex52 <kapex@bk.ru>
2022-08-25 16:28:40 +0300

    dz6 1

---
+++ DZ_C/DZ_6/DZ_6_1/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Console.WriteLine("Введите сколко вы хотите ввеси чисел");
+// int size = Convert.ToInt32(Console.ReadLine()); 
+//double[] array =new double[]  {Console.ReadKey().KeyChar != '#'};
+// int count= 0;
+// for (int i = 0; i < size; i++)
+// {
+//     Console.WriteLine($"Введите {i+1} число( не целые числа вводить через ','");
+//     array[i] = Convert.ToDouble(Console.ReadLine());  
+//     Console.WriteLine(array[i] + " ");
+
+// }
+    //do { } while (Console.ReadKey().KeyChar != '#');
+
+//   static void Main(string[] args)
+//         {
+//             int[] mas = new int[100];
+//             int i = 0; 
+//             string test;
+//             int count = 0;
+ 
+//             do
+//             {
+//                 test = Console.ReadLine();
+//                 if (test == "end") break;
+//                 mas[i] = Convert.ToInt32(test);
+//                 ++i; ++count;
+//             } while (true);
+            
+//             Console.WriteLine("Vivod: ");
+//             for (int j = 0; j < count; j++)
+//             {
+//                 Console.Write("{0} ",mas[j]);
+//             }
+//             Console.ReadLine();
+//         }
+
+ static void Main(string[] args)
+        {
+            int[] mas = new int[100];
+            int i = 0; 
+            string test;
+            int count = 0;
+ 
+            do
+            {
+                test = Console.ReadLine();
+                if (test == "end") break;
+                mas[i] = Convert.ToInt32(test);
+                ++i; ++count;
+            } while (true);
+            
+            Console.WriteLine("Vivod: ");
+            for (int j = 0; j < count; j++)
+            {
+                Console.Write("{0} ",mas[j]);
+            }
+            Console.ReadLine();
+        }
