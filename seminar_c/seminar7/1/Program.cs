3e38746f1af3b6eaf3b0b862899fc5cf5065a798
kapex52 <kapex@bk.ru>
2022-08-26 11:44:26 +0300

    s7_2

---
+++ seminar_c/seminar7/1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+// m - строчки, n - столбцы
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++) // по строчкам
+    {
+        // i, j, m, k
+        for (int j = 0; j < matrix.GetLength(1); j++) // по столбцам
+        {
+            matrix[i, j] = new Random().Next(11); // [0;10]
+        }
+    }
+    return matrix;
+}
+int[,] resultMatrix = GetArray(rows, columns);
+PrintArray(resultMatrix);
+void PrintArray(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, j] + "\t"); // "\t" - Tab
+        }
+        Console.WriteLine();
+    }
+}
---
+++ seminar_c/seminar7/2/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m + n.Выведите полученный массив на экран.
+//m = 3, n = 4.
+//0 1 2 3
+//1 2 3 4
+//2 3 4 5
+
+//Console.WriteLine("Введите два числа: ");
+//int rows = Convert.ToInt32(Console.ReadLine());
+//int columns = Convert.ToInt32(Console.ReadLine());
+
+//int[,] numbers = new int[rows, columns];
+
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < columns; j++)
+//    {
+//        numbers[i, j] = i + j;
+//        Console.Write(numbers[i, j] + "\t");
+//    }
+//    Console.WriteLine();
+//}
+
+//Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+//Console.WriteLine("Введите два числа: ");
+//int rows = Convert.ToInt32(Console.ReadLine());
+//int columns = Convert.ToInt32(Console.ReadLine());
+
+//int[,] numbers = new int[rows, columns];
+
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < columns; j++)
+//    {
+//        numbers[i, j] = new Random().Next(11);
+//        if (i % 2 == 0 && j % 2 == 0)
+//        {
+//            numbers[i, j] = numbers[i, j] * numbers[i, j];
+//        }
+//        Console.Write(numbers[i, j] + "\t");
+//    }
+//    Console.WriteLine();
+//}
+
+//Задача 51.Найти сумму элементов главной диагонали.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Сумма элементов главной диагонали: 1 + 9 + 2 = 12
+
+
+Console.WriteLine("Введите два числа: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[rows, columns];
+int sum = 0;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        numbers[i, j] = new Random().Next(11);
+        if (i == j)
+        {
+            sum = sum + numbers[i, j];
+        }
+        Console.Write(numbers[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(sum);
+           
