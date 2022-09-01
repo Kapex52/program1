2d7af734fcfc46dd6acf2623d78e925a118e7ee5
kapex52 <kapex@bk.ru>
2022-09-01 23:46:12 +0300

    DZ_8

---
+++ DZ_C/DZ_8/DZ_8_3/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int n = 2;
+int[,] firstArray = new int[n, n];
+int[,] secomdArray = new int[n, n];
+int[,] resultMatrix = new int[n, n];
+
+void ProizvedenieArray(int[,] firstArray, int[,] secomdArray, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstArray.GetLength(1); k++)
+            {
+                sum += firstArray[i, k] * secomdArray[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+CreateArray(firstArray);
+Console.WriteLine();
+Console.WriteLine("Первая матрица:");
+WriteArray(firstArray);
+CreateArray(secomdArray);
+Console.WriteLine();
+Console.WriteLine("Вторая матрица:");
+WriteArray(secomdArray);
+ProizvedenieArray(firstArray, secomdArray, resultMatrix);
+Console.WriteLine();
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
---
+++ DZ_C/DZ_8/DZ_8_4/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int n = 2;
+int[,,] array = new int[n, n, n];
+
+void CreateArray(int[,,] array)
+{
+    int[] tempArray = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int number;
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        tempArray[i] = new Random().Next(10, 100);
+        number = tempArray[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (tempArray[i] == tempArray[j])
+                {
+                    tempArray[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = tempArray[i];
+                }
+                number = tempArray[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x, y, z] = tempArray[count];
+                count++;
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+CreateArray(array);
+PrintArray(array);
